ad470ca121921d182ef3f9773e4e28aad2b4643d
bryan Josue <sidebackemail@gmail.com>
2018-01-26 13:33:08 -0500

    Agregando Connection String al archivo App.config, creando ApplicationContextDb y la Entidad User

---
+++ MVC5/Data Access/Entities/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Access.Entities
+{
+    [Table("Usuario")]
+    public class User
+    {
+        [Key]
+        [Column(Order = 0)]
+        public Guid UserId { get; set; }
+
+        [StringLength(16,MinimumLength = 8)]
+        [Required]
+        [Column(Order = 1)]
+        public string Password { get; set; }
+
+        [StringLength(50)]
+        [Required]
+        [Column(Order = 2)]
+        public string UserName { get; set; }
+
+        [Required]
+        [Column(Order = 3)]
+        public string Email { get; set; }
+
+        public byte[] Avatar { get; set; }
+    }
+}
